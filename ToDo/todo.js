65f87a591866c8acab2925f68d6cc7a2a87f34f9
Hannah Krager <hannahkrager@gmail.com>
2015-07-11 09:26:47 -0700

    switch to jquery

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -1,31 +1,11 @@
-(function(){
-    window.list = document.getElementById('lists');
-    var form = document.getElementById('form');
-    var input = document.getElementById('task');
+$(function(){
 
-    form.addEventListener("submit", function(event){
-        var task = input.value
-        //while there is text input, create and add a list element
-        if (task != '') {
-            var li = document.createElement("li");
-            var text = document.createTextNode(task);
-            li.appendChild(text);
-            //window.list.appendChild(li);
 
-            window.list.insertBefore(li, window.list.firstChild);
-
-            input.value = null;
-        }
-        //prevent empty list elements from being added
-        event.preventDefault();
+    $('li').on('click', 'li', function(){
+        $(this).addClass('remove');
+        $('.remove').remove();
     });
 
-    list.addEventListener('click', function(event){
-    var tag = event.target;
-    if (tag.tagName === 'LI') {
-      tag.parentNode.removeChild(tag);
-    };
-    event.preventDefault();
-    });
 
-})();
+});
+
