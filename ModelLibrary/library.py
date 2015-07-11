e309f901ffc1eb836bf3c1945cb09727b508f1b2
Hannah Krager <hannahkrager@gmail.com>
2015-07-11 12:09:28 -0700

    final functionality

--- ModelLibrary/library.py
+++ ModelLibrary/library.py
@@ -1,61 +1,60 @@
-#The library should be aware of a number of distinct shelves.
-#Create methods to add and remove a book from a self.
-#The library should have a method to report all books it contains.
 
 class Library(object):
     """create Library object"""
     biblioteca = {}
 
-    def book_count(total_books):
+    def book_count():
         """Number of shelves in Library"""
         return len(Library.biblioteca)
 
     def bookTitles():
         """Titles of all books present in Library"""
-
-
-#method for title of every book it contains
-
+        all_titles = Library.biblioteca.values()
+        unpacked = [j for i in all_titles for j in i]
+        return ', '.join(unpacked)
 
 
 class Shelf(Library):
     """create  a shelf in library for books"""
 
-    def __init__(self, name, books=''):
+    def __init__(self, name, books):
         self.name = name
         self.books = books
+        Book(self.name, self.books)
         Library.biblioteca[self.name] = [self.books] if self.books else []
-
-    def addBook(self, newtitle):
-        Library.biblioteca[self.name].append(newtitle)
 
     def contents(self):
         shelf_contents = Library.biblioteca[self.name]
         return ', '.join(shelf_contents)
 
-    def delete(self, title):
+    def addBook(self, newtitle):
+        Library.biblioteca[self.name].append(newtitle)
 
-        shelf_contents = Library.biblioteca[self.name]
+    def delete(self, title):
+        shelf_items = Library.biblioteca[self.name]
         try:
-            shelf_contents.remove(title)
+            shelf_items.remove(title)
         except ValueError:
             return 'That book is not on this shelf.'
 
 
-
-class Book(object):
+class Book(Library):
     """add a book, associate with shelf in library"""
-    def __init__(self, title):
+    def __init__(self, shelf, title):
         self.title = title
+        self.shelf = shelf
+
+        #Shelf(self.shelf, self.title)
 
 
 if __name__ == '__main__':
     S = Shelf('Fantasy', 'The Name Of the Wind')
-    Shelf('Crap', '50 Shades of Grey')
-    Shelf('Mystery')
+    D = Shelf('Crap', '50 Shades of Grey')
+    M = Shelf('Mystery', 'Sherlock Holmes')
     S.addBook('The Stormlight Archive')
     S.delete('The Name Of the Wind')
     print(S.delete('The Way of Kings'))
     print(S.contents())
-
-    print(Library.biblioteca)
+    print(M.contents())
+    print(Library.bookTitles())
+    print(Library.book_count())
