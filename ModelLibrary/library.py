6f4ef669b07de6270a4d53d91bb931f8135f01e2
Hannah Krager <hannahkrager@gmail.com>
2015-07-17 18:28:55 -0700

    add print statements to show functionality

--- ModelLibrary/library.py
+++ ModelLibrary/library.py
@@ -1,15 +1,15 @@
 
-class Library(object):
+class Library:
     """create Library object"""
     biblioteca = {}
 
-    def book_count():
+    def book_count(self):
         """Number of shelves in Library"""
-        return len(Library.biblioteca)
+        return len(self.biblioteca)
 
-    def bookTitles():
+    def book_titles(self):
         """Titles of all books present in Library"""
-        all_titles = Library.biblioteca.values()
+        all_titles = self.biblioteca.values()
         unpacked = [j for i in all_titles for j in i]
         return ', '.join(unpacked)
 
@@ -17,11 +17,9 @@ class Library(object):
 class Shelf(Library):
     """create  a shelf in library for books"""
 
-    def __init__(self, name, books):
+    def __init__(self, name):
         self.name = name
-        self.books = books
-        Book(self.name, self.books)
-        Library.biblioteca[self.name] = [self.books] if self.books else []
+        Library.biblioteca[self.name] = []
 
     def contents(self):
         shelf_contents = Library.biblioteca[self.name]
@@ -46,13 +44,32 @@ class Book(Library):
 
 
 if __name__ == '__main__':
-    S = Shelf('Fantasy', 'The Name Of the Wind')
-    D = Shelf('Crap', '50 Shades of Grey')
-    M = Shelf('Mystery', 'Sherlock Holmes')
-    S.addBook('The Stormlight Archive')
-    S.delete('The Name Of the Wind')
-    print(S.delete('The Way of Kings'))
-    print(S.contents())
-    print(M.contents())
-    print(Library.bookTitles())
-    print(Library.book_count())
+    broadview = Library()
+
+    F = Shelf('Fantasy')
+    F.addBook('The Dresden Files')
+    F.addBook('The Name Of the Wind')
+
+    G = Shelf('Garbage')
+    G.addBook('50 Shades of Grey')
+
+    M = Shelf('Mystery')
+    M.addBook('Sherlock Holmes')
+
+    amt = broadview.book_count()
+    print("This library currently has {} shelves.\n".format(amt))
+
+    total = Library.book_titles
+    print("this library currently holds the following titles: {}".format(total))
+
+    fan = F.contents()
+    print("The 'Fantasy' shelf currently holds: {}\n".format(fan))
+
+    F.addBook('The Stormlight Archive')
+    fan = F.contents()
+    print("After adding a book, 'Fantasy' now has: {}".format(fan))
+
+    F.delete('The Name Of the Wind')
+    fan = F.contents()
+    print("After deleting a book, 'Fantasy' now has: {}".format(fan))
+
